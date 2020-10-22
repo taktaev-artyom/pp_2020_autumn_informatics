77ac47a3c311a7a2688447877dd0898ff7c71d05
taktaev-artyom <43129320+taktaev-artyom@users.noreply.github.com>
2020-10-22 16:34:52 +0300

    fix 1

--- modules/task_1/taktaev_a_vector_adj_alternations/vector_adj_alternations.cpp
+++ modules/task_1/taktaev_a_vector_adj_alternations/vector_adj_alternations.cpp
@@ -5,15 +5,18 @@
 #include "../../../modules/task_1/taktaev_a_vector_adj_alternations/vector_adj_alternations.h"
 
 std::vector<int> createRandomVector(int vec_size) {
+    if (vec_size <= 0) throw "Wrong size";
     std::random_device rand;
     std::mt19937 gen(rand());
     std::vector<int> res_vec(vec_size);
-    for (int  i = 0; i < vec_size; i++) 
+    for (int  i = 0; i < vec_size; i++)
 	    res_vec[i] = static_cast<int>(gen() % 201) - 100;
     return res_vec;
 }
 
 int calculateAdjAlternationsSequential(const std::vector<int> &vec, int inc, int start_index) {
+    if (start_index < 1) throw "Wrong start index";
+    if (inc < 1) throw "Wrong increment";
     int count = 0;
     for (int i = start_index; i < vec.size(); i = i + inc)
         if (vec[i] * vec[i - 1] < 0) count++;
@@ -22,7 +25,7 @@ int calculateAdjAlternationsSequential(const std::vector<int> &vec, int inc, int
 
 int calculateAdjAlternationsParallel(const std::vector<int> &vec, int vec_size) {
     int procNum, procRank;
-    //if (vec_size <= 0) throw "Wrong size";
+    if (vec_size <= 0) throw "Wrong size";
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     int part_vec_size = vec_size / procNum;
--- modules/task_1/taktaev_a_vector_adj_alternations/vector_adj_alternations.h
+++ modules/task_1/taktaev_a_vector_adj_alternations/vector_adj_alternations.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Taktaev Artem
-#ifndef MODULES_TASK_1_TAKTAEV_A_VECTOR_ADJ_ALTERNATIONS_VECTOR_ADJ_ALTERNATIONS_H
-#define MODULES_TASK_1_TAKTAEV_A_VECTOR_ADJ_ALTERNATIONS_VECTOR_ADJ_ALTERNATIONS_H
+#ifndef MODULES_TASK_1_TAKTAEV_A_VECTOR_ADJ_ALTERNATIONS_VECTOR_ADJ_ALTERNATIONS_H_
+#define MODULES_TASK_1_TAKTAEV_A_VECTOR_ADJ_ALTERNATIONS_VECTOR_ADJ_ALTERNATIONS_H_
 
 #include <vector>
 
@@ -8,4 +8,4 @@ std::vector<int> createRandomVector(int  vec_size);
 int calculateAdjAlternationsParallel(const std::vector<int> &vec, int vec_size);
 int calculateAdjAlternationsSequential(const std::vector<int> &vec, int inc, int start_index);
 
-#endif  // MODULES_TASK_1_TAKTAEV_A_VECTOR_ADJ_ALTERNATIONS_VECTOR_ADJ_ALTERNATIONS_H
+#endif  // MODULES_TASK_1_TAKTAEV_A_VECTOR_ADJ_ALTERNATIONS_VECTOR_ADJ_ALTERNATIONS_H_
