27c96b99291f25737f6d47829312b183964cbb5a
emelkhovsky <emelkhovsky4@mail.ru>
2020-12-05 02:15:29 +0300

    test update

---
+++ modules/task_1/emelkhovsky_k_integration_monte_carlo/integration_monte_carlo.h
@@ -0,0 +1,10 @@
+// Copyright 2020 Emelkhovsky Ekaterina
+#ifndef MODULES_TASK_1_EMELKHOVSKY_K_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
+#define MODULES_TASK_1_EMELKHOVSKY_K_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
+
+int funcF(int x);
+int funcSin(int x);
+double ParallelFunc(int N, int a, int b, int fl);
+double EasyFunc(int N, int a, int b, int fl);
+
+#endif  // MODULES_TASK_1_EMELKHOVSKY_K_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
