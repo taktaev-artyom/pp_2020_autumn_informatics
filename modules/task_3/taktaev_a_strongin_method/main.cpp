3cf1ba1bcee72690b0e4fb144f1e22f4d31c5d34
taktaev-artyom <artaktaev@gmail.com>
2020-12-09 16:48:17 +0300

    included cmath sin in std:)

--- modules/task_3/taktaev_a_strongin_method/main.cpp
+++ modules/task_3/taktaev_a_strongin_method/main.cpp
@@ -3,33 +3,34 @@
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <cmath>
 #include "../../../modules/task_3/taktaev_a_strongin_method/strongin_method.h"
 
 TEST(Strongin_Method_MPI, Test_Wrong_Parameters_In_Constructor) {
-    ASSERT_ANY_THROW(Strongin X(2, -2, 2, -4, [](double x)->double { return sin(x); }));
+    ASSERT_ANY_THROW(Strongin X(2, -2, 2, -4, [](double x)->double { return std::sin(x); }));
 }
 
 TEST(Strongin_Method_MPI, Test_Correct_Parameters_In_Constructor) {
-    ASSERT_NO_THROW(Strongin X(-1, 1, 2, -4, [](double x)->double { return sin(x); }));
+    ASSERT_NO_THROW(Strongin X(-1, 1, 2, -4, [](double x)->double { return std::sin(x); }));
 }
 
 TEST(Strongin_Method_MPI, Test_Wrong_X_In_add_Node) {
-    Strongin X(-1, 1, 2, -4, [](double x)->double { return sin(x); });
+    Strongin X(-1, 1, 2, -4, [](double x)->double { return std::sin(x); });
     ASSERT_ANY_THROW(X.addNode(5));
 }
 
 TEST(Strongin_Method_MPI, Test_Correct_X_In_add_Node) {
-    Strongin X(-1, 1, 2, -4, [](double x)->double { return sin(x); });
+    Strongin X(-1, 1, 2, -4, [](double x)->double { return std::sin(x); });
     ASSERT_NO_THROW(X.addNode(0.5));
 }
 
 TEST(Strongin_Method_MPI, Test_Correct_Work_Of_seq_Strongin_Method) {
-    Strongin Y(-1, 1, 2, -10, [](double x)->double { return sin(x); });
+    Strongin Y(-1, 1, 2, -10, [](double x)->double { return std::sin(x); });
     ASSERT_NEAR(Y.seqStronginSearch(), -1, 1.0e-10);
 }
 
 TEST(Strongin_Method_MPI, Test_Correct_Work_Of_par_Strongin_Method) {
-    Strongin Z(-1, 1, 2, -10, [](double x)->double { return sin(x); });
+    Strongin Z(-1, 1, 2, -10, [](double x)->double { return std::sin(x); });
     ASSERT_NEAR(Z.parStronginSearch(), -1, 1.0e-10);
 }
 
