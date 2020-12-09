ae592b287dd1bcf04e78a3f9bfe4f8f46bcf4e0a
Maria Bakaeva <mary396969@gmail.com>
2020-12-09 22:04:35 +0300

    version1

---
+++ modules/task_3/bakaeva_m_integrals_rectangles_method/integrals_rectangles_method.h
@@ -0,0 +1,12 @@
+// Copyright 2020 Bakaeva Maria
+#ifndef MODULES_TASK_3_BAKAEVA_M_INTEGRALES_RECTANGLES_METHOD_INTEGRALES_RECTANGLES_METHOD_H_
+#define MODULES_TASK_3_BAKAEVA_M_INTEGRALES_RECTANGLES_METHOD_INTEGRALES_RECTANGLES_METHOD_H_
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::pair;
+
+double getSequentialIntegrals(const int n, vector<pair<double, double> > a_b, double (*F)(vector<double>));
+double getParallelIntegrals(const int n, vector<pair<double, double> > a_b, double (*F)(vector<double>));
+#endif  // MODULES_TASK_3_BAKAEVA_M_INTEGRALES_RECTANGLES_METHOD_INTEGRALES_RECTANGLES_METHOD_H_
