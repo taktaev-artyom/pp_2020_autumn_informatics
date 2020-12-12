0b77a19881d53ecbb277631e461e1203d63214ea
Nikita Kuznetsov <nikita0bond@gmail.com>
2020-12-12 11:03:50 +0300

    ...

--- modules/task_3/kuznetsov_mult_sparse_mat/mult_sparse_mat.h
+++ modules/task_3/kuznetsov_mult_sparse_mat/mult_sparse_mat.h
@@ -4,4 +4,17 @@
 
 #include <vector>
 
+std::vector<double> randMat(const int rows, const int cols);
+
+struct sparseMatrix {
+  std::vector<double> val;
+  std::vector<int> c_rows;
+  std::vector<int> c_index;
+  int cols, rows, nnz;
+  friend const std::vector<double> operator*(const sparseMatrix& A, const sparseMatrix& B);
+};
+
+sparseMatrix CCS(const std::vector<double> new_mat, const int _cols, const int _rows);
+std::vector<double> matMultiply(sparseMatrix A, sparseMatrix B);
+
 #endif  // MODULES_TASK_3_KUZNETSOV_MULT_SPARSE_MAT_MULT_SPARSE_MAT_H_
