0cd3c4bc90092cd1c2053ce9a3a145d98824c563
gurylev-nikita <gurylev.nickita@yandex.ru>
2020-12-16 17:22:59 +0300

    done

---
+++ modules/task_3/gurylev_n_integral_monte_carlo/integral_monte_carlo.h
@@ -0,0 +1,16 @@
+// Copyright 2020 Gurylev Nikita
+#ifndef MODULES_TASK_3_GURYLEV_N_INTEGRAL_MONTE_CARLO_INTEGRAL_MONTE_CARLO_H_
+#define MODULES_TASK_3_GURYLEV_N_INTEGRAL_MONTE_CARLO_INTEGRAL_MONTE_CARLO_H_
+
+#include <vector>
+#include <functional>
+
+double getSequentialIntegralMCarlo(double(*function)(std::vector<double>),
+    const std::vector<double>& llim,
+    const std::vector<double>& ulim, int n);
+
+double getParallelIntegralMCarlo(double(*function)(std::vector<double>),
+    const std::vector<double>& llim,
+    const std::vector<double>& ulim, int n);
+
+#endif  // MODULES_TASK_3_GURYLEV_N_INTEGRAL_MONTE_CARLO_INTEGRAL_MONTE_CARLO_H_
