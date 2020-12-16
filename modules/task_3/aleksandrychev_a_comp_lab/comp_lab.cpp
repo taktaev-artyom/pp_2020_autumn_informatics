935b87551b550fab8088ea62e7d9e05e89aefa14
Andrew <andrey.aleksandrychev2@mail.ru>
2020-12-16 23:50:40 +0300

    Third commit

--- modules/task_3/aleksandrychev_a_comp_lab/comp_lab.cpp
+++ modules/task_3/aleksandrychev_a_comp_lab/comp_lab.cpp
@@ -8,6 +8,8 @@
 
 #include "../../modules/task_3/aleksandrychev_a_comp_lab/comp_lab.h"
 
+// Third commit
+
 std::vector<uint32_t> getRandomBinaryImage(int height, int width) {
     if (height < 1 || width < 1) {
         throw "height or width < 1 error";
