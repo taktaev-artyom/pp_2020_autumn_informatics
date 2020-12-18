4d5b098c678939719c4889b2524040afb3354b79
lutovatanya <lutovat20@gmail.com>
2020-12-18 14:39:34 +0300

    END END END

--- modules/task_3/lyutova_t_quick_sort/quick_sort.h
+++ modules/task_3/lyutova_t_quick_sort/quick_sort.h
@@ -8,6 +8,6 @@ std::vector<int> getRandomVector(int size);
 void quickSortImplementation(std::vector<int>* array, int a, int b);
 std::vector<int> quickSortSequential(std::vector<int> arr);
 std::vector<int> mergeSort(std::vector<int> arr1, std::vector<int> arr2);
-std::vector<int> quickSortParallel(const std::vector<int> array);
+std::vector<int> quickSortParallel(const std::vector<int> arr);
 
 #endif  // MODULES_TASK_3_LYUTOVA_T_QUICK_SORT_QUICK_SORT_H_
