d44d318a8af9dc67787982b174f4f914f762dda1
“Roman <roman.zhuravlev2911@gmail.com>
2020-12-18 22:35:07 +0300

    3

--- modules/task_3/zhuravl_r_simpson/main.cpp
+++ modules/task_3/zhuravl_r_simpson/main.cpp
@@ -1,24 +1,24 @@
 // Copyright 2020 Zhuravlev Roman
 #include <gtest-mpi-listener.hpp>
 #include <gtest/gtest.h>
-#include "../../../modules/task_3/zhuravlev_r_simpson/Simpson.h"
+#include "./Simpson.h"
 
-TEST(Parallel_MPI, Test_Sequantial_Res) {
+TEST(Parallel_MPI, Test_Sequantial_Result) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   if (rank == 0) {
-    EXPECT_DOUBLE_EQ(Sequential_Simpson_Double(func, 0, 3, -2, 4, 10, 8), 10.8) << "Wrong res";
+    EXPECT_DOUBLE_EQ(Sequential_SimpsonForDouble(func, 0, 3, -2, 4, 10, 8), 10.8) << "Wrong res";
   }
 }
 
-TEST(Parallel_MPI, Test_Parallel_Res) {
+TEST(Parallel_MPI, Test_Parallel_Result) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  double res = Parallel_Simpson_Double(func, 1, 14, 2, 5, 10, 10);
+  double result = Parallel_SimpsonForDouble(func, 1, 14, 2, 5, 10, 10);
 
   if (rank == 0) {
-    EXPECT_DOUBLE_EQ(res, 409.5) << "Wrong res";
+    EXPECT_DOUBLE_EQ(result, 409.5) << "Wrong res";
   }
 }
 
@@ -27,20 +27,20 @@ TEST(Parallel_MPI, Test_Exception) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   if (rank == 0) {
-    EXPECT_ANY_THROW(Sequential_Simpson_Double(func, 0, 3, 5, 4, 10, 8));
+    EXPECT_ANY_THROW(Sequential_SimpsonForDouble(func, 0, 3, 5, 4, 10, 8));
   }
 }
 
-TEST(Parallel_MPI, Test_Parallel_Res_equals_Sequential_Res_small) {
+TEST(Parallel_MPI, Test_Parallel_Result_equals_Sequential_Result_small) {
   int rank;
   double t1, t2, t3, t4;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   t1 = MPI_Wtime();
-  double parRes = Parallel_Simpson_Double(func, 0, 3, -2, 4, 30, 20);
+  double parRes = Parallel_SimpsonForDouble(func, 0, 3, -2, 4, 30, 20);
   t2 = MPI_Wtime();
   t3 = MPI_Wtime();
-  double seqRes = Sequential_Simpson_Double(func, 0, 3, -2, 4, 30, 20);
+  double seqRes = Sequential_SimpsonForDouble(func, 0, 3, -2, 4, 30, 20);
   t4 = MPI_Wtime();
   if (rank == 0) {
     std::cout << "parallel: " << (t2 - t1) << ", seqential: " << (t4 - t3) << std::endl;
@@ -48,16 +48,16 @@ TEST(Parallel_MPI, Test_Parallel_Res_equals_Sequential_Res_small) {
   }
 }
 
-TEST(Parallel_MPI, Test_Parallel_Res_equals_Sequential_Res_big) {
+TEST(Parallel_MPI, Test_Parallel_Result_equals_Sequential_Result_big) {
   int rank;
   double t1, t2, t3, t4;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   t1 = MPI_Wtime();
-  double parRes = Parallel_Simpson_Double(func, -10, 31, 1, 15, 100, 80);
+  double parRes = Parallel_SimpsonForDouble(func, -10, 31, 1, 15, 100, 80);
   t2 = MPI_Wtime();
   t3 = MPI_Wtime();
-  double seqRes = Sequential_Simpson_Double(func, -10, 31, 1, 15, 100, 80);
+  double seqRes = Sequential_SimpsonForDouble(func, -10, 31, 1, 15, 100, 80);
   t4 = MPI_Wtime();
   if (rank == 0) {
     std::cout << "parallel: " << (t2 - t1) << ", seqential: " << (t4 - t3) << std::endl;
